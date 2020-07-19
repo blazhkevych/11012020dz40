64bb138393d321ee6bba1c0e0a028048566014d8
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:56:29 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz40.cpp
@@ -0,0 +1,18 @@
+﻿/*
+* У 	дворника почасовая оплата труда.Пользователь вводит ставку за час и 	желаемую сумму зарплаты.Посчитать, сколько часов надо отработать дворнику.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	double a, b, c;
+	cout << "Введите з / п за час: " << endl;
+	cin >> a;
+	cout << "Введите желаемую сумму зп: " << endl;
+	cin >> b;
+	c = b / a;
+	cout << c << endl;
+	return 0;
+}
